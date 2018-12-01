038bb5dd3d7edf93a81ffe1247f9794d0616d223
Guillaume <hi@255kb.com>
2018-12-01 15:09:59 +0100

    Add type compatibility examples in inference

--- examples/inference.ts
+++ examples/inference.ts
@@ -47,3 +47,44 @@ window.onmouseover = function (mouseEvent) {
   mouseEvent.clientX;
   mouseEvent.anything; // <-- Error
 }
+
+
+/** TYPE COMPATIBILITY */
+
+interface Animal {
+  type: string;
+  legs: number;
+}
+
+class Bird {
+  type: string;
+  legs: number;
+}
+
+let seagul: Animal;
+seagul = new Bird();// <-- ok because interface and class are compatible even if Bird does not implements Animal
+
+
+
+
+interface Named {
+  name: string;
+}
+
+let person: Named;
+let employee1 = { name: 'Steven', lastname: 'seagul' };
+person = employee1; // <-- ok because Named properties are all in employee1 infered type
+
+
+enum Enum1 {
+  Red,
+  Green
+}
+enum Enum2 {
+  Low,
+  Medium, 
+  High
+}
+
+let a = Enum1.Red;
+a = Enum2.Low;// <-- error Enum1.Red not assignable to Enum2.Low
