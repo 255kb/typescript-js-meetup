66ee5b107bc92a9ea28f7a398018b9ed4816e0ce
Guillaume Monnet <guillaume.monnet@reborn.lu>
2018-12-05 16:41:01 +0100

    Recompile class example

--- dist/member.d.ts
+++ dist/member.d.ts
@@ -1,7 +1,7 @@
-interface Human {
+interface User {
     age: number | null;
 }
-export declare class Member implements Human {
+export declare class Member implements User {
     age: number | null;
     constructor(age: number | null);
     hasMajority(): boolean;
